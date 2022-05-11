016cbe0423fa1bb2f71155f8923def986155ef25
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-05-11 23:23:20 +0300

    Create textgen.h

---
+++ include/textgen.h
@@ -0,0 +1,19 @@
+// Copyright 2022 UNN-IASR
+#pragma once
+#include <string>
+#include <deque>
+#include <vector>
+#include <map>
+using namespace std;
+class genText {
+public:
+    string data;
+    string getText();
+
+    std::vector<string> words;
+    typedef std::deque<string> prefix;
+    map<prefix, vector<string> > statetab;  
+    int NPREF;  
+    int MAXGEN;  
+    genText(string filename, int i1, int i2);
+};
