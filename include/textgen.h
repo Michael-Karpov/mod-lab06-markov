0c3a8c8c0d9e2635d82beb79a06ecc41477ac702
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-05-12 00:48:06 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -1,18 +1,27 @@
 // Copyright 2022 UNN-IASR
 #pragma once
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <vector>
 #include <deque>
 #include <map>
-#include <string>
-#include <vector>
+#include <fstream>
+#define rand_r rand
 
-class Gen {
+const int NPREF = 2;
+const int MAXGEN = 1000;
+
+class MarkovGenerator {
  private:
-  typedef std::deque<std::string> prefix;
-  std::map<prefix, std::vector<std::string> > statetab;
-  int NPREF = 2;
-  int MAXGEN = 1000;
+    int prefixSize;
+    typedef std::deque<std::string> prefix;
+    std::map<prefix, std::vector<std::string> > statetab;
+    std::vector<std::string> words;
  public:
-  std::string Gen();
-  Gen(std::string file);
-  std::string generate();
+    MarkovGenerator(std::string path, int preflen);
+    std::string getText(int wordsamount);
+    int getPrefixSize();
+    std::string getSuffix(std::deque<std::string> prefdeq);
 };
