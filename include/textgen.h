9dfe39b6c7b3bc33bdee6ccc98fdc8b8e53bc846
Mihail Karpov <100800994+Michael-Karpov@users.noreply.github.com>
2022-05-12 00:13:10 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -1,19 +1,17 @@
 // Copyright 2022 UNN-IASR
 #pragma once
-#include <string>
 #include <deque>
-#include <vector>
 #include <map>
-
-class genText {
+#include <string>
+#include <vector>
+using namespace std;
+class Gen {
+ private:
+ typedef deque<string> prefix;
+ map<prefix, vector<string>> statetab;
+ int NPREF = 2;
+ int MAXGEN = 1000;
  public:
-    string data;
-    string getText();
-
-    std::vector<string> words;
-    typedef std::deque<string> prefix;
-    map<prefix, vector<string> > statetab;
-    int NPREF;
-    int MAXGEN;
-    genText(string filename, int i1, int i2);
+ Gen(string file);
+ string generate();
 };
